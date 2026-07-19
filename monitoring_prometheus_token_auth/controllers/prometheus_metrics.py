2a5320182e489e3c33364991803143105b6d8b87
Ross Golder <ross@golder.org>
2026-07-19 16:19:53 +0700

    restore: monitoring_prometheus_token_auth (lost in camptocamp/19.0 merge)
    This module was created by splitting out the token auth logic from
    monitoring_prometheus. It was lost when camptocamp/19.0 was merged
    into 17.0 (commit d5b0b7a) since upstream never had this module.
    
    Restored from 17.0-develop branch (commit 9f0ac8a).

---
+++ monitoring_prometheus_token_auth/controllers/prometheus_metrics.py
@@ -0,0 +1,13 @@
+# Copyright 2016-2021 Camptocamp SA
+# Copyright 2025 Ross Golder
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+from prometheus_client import generate_latest
+
+from odoo.http import Controller, route, request
+
+class PrometheusController(Controller):
+    @route("/metrics", auth='token')
+    def metrics(self, **kw):
+        headers = {'Content-Type': 'text/plain'}
+        return request.make_response(generate_latest(), headers=headers)
